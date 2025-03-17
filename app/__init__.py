c93647ba1903abeb5fb400f8c5ce066a09ec9772
Astro026 <100047672+Astro026@users.noreply.github.com>
2025-03-17 00:12:40 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ app.secret_key = '1223absc'
 lm = LoginManager(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:226226uy@astrocode.cdee0eaoc82b.us-east-2.rds.amazonaws.com:3306/grupo_mar'
 app.config['SQLALCHEMY_BINDS'] = {
-    'estoque_producao':''mysql+pymysql://admin:226226uy@astrocode.cdee0eaoc82b.us-east-2.rds.amazonaws.com:3306/estoque_producao',
+    'estoque_producao':'mysql+pymysql://admin:226226uy@astrocode.cdee0eaoc82b.us-east-2.rds.amazonaws.com:3306/estoque_producao',
    }
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
